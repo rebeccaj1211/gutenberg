56074d66118b9d5c02a4936170eda9d0b2ad5d3d
iseulde <wp@iseulde.com>
2019-04-10 16:51:54 +0200

    Do not modify live range in getRectangleFromRange

--- packages/dom/src/dom.js
+++ packages/dom/src/dom.js
@@ -218,6 +218,8 @@ export function getRectangleFromRange( range ) {
 	// See: https://stackoverflow.com/a/6847328/995445
 	if ( ! rect ) {
 		const padNode = document.createTextNode( '\u200b' );
+		// Do not modify to live range.
+		range = range.cloneRange();
 		range.insertNode( padNode );
 		rect = range.getClientRects()[ 0 ];
 		padNode.parentNode.removeChild( padNode );
